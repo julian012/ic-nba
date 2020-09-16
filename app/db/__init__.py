5d64acae39c6016f861d194c0a46ef63e1e04dba
Julián Camilo Serna Vargas <julicamnet@gmail.com>
2020-09-16 14:58:28 -0500

    set heroku database credentials

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,7 +2,15 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost/nba'
+HOST = 'ec2-35-172-73-125.compute-1.amazonaws.com'
+DATABASE = 'dbhbu2mce42c2h'
+USER = 'lpbokbiofopknm'
+PASSWORD = '94efb703ce2d583eb7eac19d8e35f9ef304d45de091345a6061f77f5a133a1cd'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lpbokbiofopknm' \
+                                        ':94efb703ce2d583eb7eac19d8e35f9ef304d45de091345a6061f77f5a133a1cd@ec2-35-172' \
+                                        '-73-125.compute-1.amazonaws.com:5432/dbhbu2mce42c2h'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
