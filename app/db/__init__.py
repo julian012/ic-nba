5ec8f9150b2c2e0604d476f1b9e3a3e6c41a9378
Julián Camilo Serna Vargas <julicamnet@gmail.com>
2020-09-16 19:24:57 -0500

    Change server database to ElephantSQL

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -8,9 +8,7 @@ USER = 'lpbokbiofopknm'
 PASSWORD = '94efb703ce2d583eb7eac19d8e35f9ef304d45de091345a6061f77f5a133a1cd'
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lpbokbiofopknm' \
-                                        ':94efb703ce2d583eb7eac19d8e35f9ef304d45de091345a6061f77f5a133a1cd@ec2-35-172' \
-                                        '-73-125.compute-1.amazonaws.com:5432/dbhbu2mce42c2h'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://zmvsxfgv:IIq9KxS2JnoLFsEAAB8z7nB4_adR_SXb@lallah.db.elephantsql.com:5432/zmvsxfgv'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
